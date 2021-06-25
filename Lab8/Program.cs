d5ac7b8055603e5fe02e6816b8640d2ab76b4bbd
t2009m1cuong <cuongnmth2009037@fpt.edu.vn>
2021-06-25 07:50:11 +0700

    up file lab8

---
+++ Lab8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab8
+{
+    internal class Program
+    {
+        //Exercise 1:
+        public static int GreaterCount(List<double> temperatures, double min)
+        {
+            int count = 0;
+            foreach (var content in temperatures)
+            {
+                if (content > min)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        //Exercise 2:
+        static int GreaterCount(IEnumerable<double> eble, double min)
+        {
+            int count = 0;
+            Console.WriteLine("Max value using IEnumerable: " + eble.Max());
+            foreach (var content in eble)
+            {
+                if (content >= min)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        public static void Main(string[] args)
+        {
+            List<double> temperatures = new List<double>();
+            temperatures.Add(24.8);
+            temperatures.Add(5.5);
+            temperatures.Add(60);
+            temperatures.Add(25.2);
+            temperatures.Add(55.3);
+            Console.WriteLine("No of count Which is Greater than or equle to 25 degree: " + GreaterCount((IEnumerable<double>)temperatures, 25));
+            Console.ReadLine();
+
+        }
+    }
+}
